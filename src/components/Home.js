bab4cdf348e0c3b0eacadf41b6cde10fa4121a57
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-08-28 01:13:18 +0530

    google-clone

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Home.css";
+import Search from "./Search";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">Tushant</Link>
+          <Link to="/store">Store</Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="logo"
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
